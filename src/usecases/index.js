5158db94679b33d8cb7b3383a481e08198793187
Marcelo Maturana <matutosistem@gmail.com>
2024-04-28 14:48:54 -0400

    primer commit

---
+++ src/usecases/index.js
@@ -0,0 +1,6 @@
+export { crearDeck } from '../usecases/crear-deck';
+export { crearCartaHTML } from '../usecases/crear-carta-html'
+export { pedirCarta } from '../usecases/pedir-carta';
+export { turnoComputadora } from '../usecases/turno-computadora'
+export { valorCarta } from '../usecases/valor-carta';
+
---
+++ src/usecases/valor-carta.js
@@ -0,0 +1,13 @@
+// pedirCarta();
+/**
+ * Obtener el valor de la carta
+ * @param {String} carta 
+ * @returns {Number} valor de al carta
+ */
+export const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+}
